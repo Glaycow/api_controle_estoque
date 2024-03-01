6a873b81cf5aa12cdc2cfdc462e8f539e9e20039
Glaycow Silveira Silva e Souza <gft-glaycow.souza@ailos.coop.br>
2024-03-01 14:46:04 -0300

    Correção

--- src/ControleEstoque.Api/ViewModel/Produto/AlterarProdutoViewModel.cs
+++ src/ControleEstoque.Api/ViewModel/Produto/AlterarProdutoViewModel.cs
@@ -2,8 +2,10 @@
 
 namespace ControleEstoque.Api.ViewModel.Produto;
 
-public class CadastroProdutoViewModel
+public class AlterarProdutoViewModel
 {
+    [Required(ErrorMessage = "{0} deve ser informado")]
+    public Guid Id { get; set; }
     [Required(ErrorMessage = "{0} deve ser informado")]
     public string Nome { get; set; }
     [Required(ErrorMessage = "{0} deve ser informado")]
@@ -12,16 +14,16 @@ public class CadastroProdutoViewModel
     public Guid FornecedorId { get; set; }
     [Required(ErrorMessage = "{0} deve ser informado")]
     public Guid CategoriaId { get; set; }
-
+    
     public Dominio.Classes.Produto Converter()
     {
         return new Dominio.Classes.Produto
         {
+            Id = Id,
             Nome = Nome,
             TipoQuantidadeId = TipoQuantidadeId,
             FornecedorId = FornecedorId,
             CategoriaId = CategoriaId,
-
         };
     }
 }
